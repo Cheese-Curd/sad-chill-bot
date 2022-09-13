0b703f6392aabe76265686112fb8a5be3833d194
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2022-09-13 09:47:32 -0400

    MORE UPDATING OF THE SAME FILE AA-

--- commands/say.js
+++ commands/say.js
@@ -1,17 +1,16 @@
 module.exports = {
     "name": "say",
     "description": "Force SCB to say whatever you want her to say [Owner Only]",
-    execute(message, args) {
-        let msg = args; // making a new variable with the argument list
-        const toSend = msg.join(" ");
+    execute(msg, args) {
+        const toSend = args.join(" ");
         const config = require('../config.json')
-        message.delete();
+        msg.delete();
         if (args.length < 1) {
             if (config.debug)
               console.debug(`Could not find any arguments for say command.`)
-            message.author.send("Make sure to add text before you send that command!")
+            msg.author.send("Make sure to add text before you send that command!")
         } else {
-          message.channel.send(toSend);
+          msg.channel.send(toSend);
         }
     }
 }

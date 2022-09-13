fcc92cafa15b9bc1723ea6bc57fd8cebfe63f35b
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2022-09-13 09:20:21 -0400

    Updated a little, and added debug

--- commands/rng.js
+++ commands/rng.js
@@ -2,10 +2,14 @@ module.exports = {
     "name": "rng",
     "description": "Rnadom number generator `Formated $rng <min> <max>`",
     execute(message, args) {
-        let max = parseInt(args[1])
-        let min = parseInt(args[0])
+        var max = parseInt(args[1]) // I used var, cry
+        var min = parseInt(args[0])
+        var config = require('../config.json')
+        
         if (isNaN(min) || isNaN(max)) {
-            message.channel.send(`don't forget the min and max number lol`)
+            if (config.debug)
+                console.debug(`${msg.author} forgot to add min or max, args: ${arg}`)
+            message.channel.send(`Don't forget the min and or max value!`)
         } else {
             min = Math.ceil(min)
             max = Math.floor(max)

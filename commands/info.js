616df831d371f874e25ee1d56aa77a9929743198
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2022-09-15 21:30:37 -0400

    Added Embeds, trying to get buttons working

--- commands/info.js
+++ commands/info.js
@@ -1,25 +1,41 @@
 module.exports = {
     "name": "info",
     "description": "Get some info on the server and bot.",
-    execute(message, args, config, bot, embedSystem) {
-      const info = embedSystem
+    execute(msg, args, config, bot, embedSystem) {
+      const { MessageActionRow, MessageButton } = require('discord.js');
+
+      const arrows = new MessageActionRow().addCompontents(
+        new MessageButton()
+          .setCustomId("prev")
+          .setLabel("Previous")
+          .setStyle("DANGER"),
+        new MessageButton()
+          .setCustomId("next")
+          .setLabel("Next")
+          .setStyle("SUCCESS"),
+      )
+      if (msg.guild)
+      {
+        const sInfo = new embedSystem()
+          .setColor(0xff00dd)
+          .setTitle('Server Information')
+          .setAuthor({ name: 'Eliana', iconURL: 'https://cdn.discordapp.com/attachments/726665166195916831/1017608834497388574/PFP.png', url: 'https://cheese-curd.github.io' }) // me =)
+          .addFields(
+            { name: 'Server', value: `*${bot.commands.get("ping").description}*` },
+            { name: 'Member Count:', value: `*${bot.commands.get("ping").description}*` }
+          )
+          .setTimestamp()
+      }
+      const bInfo = new embedSystem()
         .setColor(0xff00dd)
-        .setTitle('Commands')
+        .setTitle('Bot Information')
         .setAuthor({ name: 'Eliana', iconURL: 'https://cdn.discordapp.com/attachments/726665166195916831/1017608834497388574/PFP.png', url: 'https://cheese-curd.github.io' }) // me =)
         .addFields(
-          { name: 'Ping', value: `*${bot.commands.get("ping").description}*` },
-          { name: 'Help', value: `*Gives a list of commands.*` },
-          { name: 'Date', value: `*${bot.commands.get("date").description}*` },
-          { name: 'Flip', value: `*${bot.commands.get("flip").description}*` },
-          { name: 'RTD', value: `*${bot.commands.get("rtd").description}*` },
-          { name: 'DM', value: `*${bot.commands.get("dm").description}*` },
-          { name: '8ball', value: `*${bot.commands.get("8ball").description}*` },
-          { name: 'RNG', value: `*${bot.commands.get("rng").description}*` },
-          { name: 'CheeseQuote', value: `*${bot.commands.get("cheesequote").description}*` },
-          { name: 'Say', value: `*${bot.commands.get("say").description}*` },
-          { name: 'WhoIs', value: `*${bot.commands.get("whois").description}*` },
-          { name: 'Reset', value: `*${bot.commands.get("reset").description}*` }
+          { name: 'Name', value: `*${bot.commands.get("ping").description}*` },
+          { name: 'Commands', value: `*${bot.commands.get("ping").description}*` }
         )
         .setTimestamp()
+
+      msg.channel.send({ content: "Test!", components: [arrows] });
     }
 }

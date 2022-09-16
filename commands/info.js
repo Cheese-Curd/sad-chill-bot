8b2cdd8a2bba1494bbf998d64fe2bff7b4fc8691
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2022-09-15 21:48:26 -0400

    fuckyoufuckyoufuckyou
    `Cannot read properties of undefined (reading 'username')`
    
    **SON OF A-**

--- commands/info.js
+++ commands/info.js
@@ -2,40 +2,42 @@ module.exports = {
     "name": "info",
     "description": "Get some info on the server and bot.",
     execute(msg, args, config, bot, embedSystem) {
-      const { MessageActionRow, MessageButton } = require('discord.js');
+      const { EmbedBuilder } = require('discord.js');
 
-      const arrows = new MessageActionRow().addCompontents(
-        new MessageButton()
-          .setCustomId("prev")
-          .setLabel("Previous")
-          .setStyle("DANGER"),
-        new MessageButton()
-          .setCustomId("next")
-          .setLabel("Next")
-          .setStyle("SUCCESS"),
-      )
+      // const arrows = new MessageActionRow().addCompontents(
+      //   new MessageButton()
+      //     .setCustomId("prev")
+      //     .setLabel("Previous")
+      //     .setStyle("DANGER"),
+      //   new MessageButton()
+      //     .setCustomId("next")
+      //     .setLabel("Next")
+      //     .setStyle("SUCCESS"),
+      // )
+      if(config.debug)
+        console.debug(`Bot Username: ${bot.username} | Commands: ${config.cmdList.length + config.oCmdList.length}`)
       if (msg.guild)
       {
-        const sInfo = new embedSystem()
+        const sInfo = new EmbedBuilder()
           .setColor(0xff00dd)
           .setTitle('Server Information')
           .setAuthor({ name: 'Eliana', iconURL: 'https://cdn.discordapp.com/attachments/726665166195916831/1017608834497388574/PFP.png', url: 'https://cheese-curd.github.io' }) // me =)
           .addFields(
-            { name: 'Server', value: `*${bot.commands.get("ping").description}*` },
-            { name: 'Member Count:', value: `*${bot.commands.get("ping").description}*` }
+            { name: 'Server', value: msg.guild.name },
+            { name: 'Member Count', value: msg.guild.memberCount }
           )
           .setTimestamp()
       }
-      const bInfo = new embedSystem()
+      const bInfo = new EmbedBuilder()
         .setColor(0xff00dd)
         .setTitle('Bot Information')
         .setAuthor({ name: 'Eliana', iconURL: 'https://cdn.discordapp.com/attachments/726665166195916831/1017608834497388574/PFP.png', url: 'https://cheese-curd.github.io' }) // me =)
         .addFields(
-          { name: 'Name', value: `*${bot.commands.get("ping").description}*` },
-          { name: 'Commands', value: `*${bot.commands.get("ping").description}*` }
+          { name: 'Name', value: bot.username }, // fuck youfuck youfuck youfuck youfuck youfuck youfuck youfuck youfuck youfuck youfuck youfuck you
+          { name: 'Commands', value: config.cmdList.length + config.oCmdList.length }
         )
         .setTimestamp()
 
-      msg.channel.send({ content: "Test!", components: [arrows] });
+        msg.channel.send({ content: "Test!", /* components: [arrows] */ embeds: [sInfo, bInfo] });
     }
 }

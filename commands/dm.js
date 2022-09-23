2b2754349033c7bee6961cfd254b531d8f948b43
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2022-09-22 22:21:58 -0400

    I don't think this will work anymore :c

--- commands/dm.js
+++ commands/dm.js
@@ -6,30 +6,31 @@ module.exports = {
         if (!msg.guild) {
             msg.author.send(`bro you're not in a server lmao`)
         } else {
-            list = client.guilds.cache.get(''); // how tf do I get the GuildID if I need to grab the guild??????????????
-            list.members.fetch().then(m => {
-                let members = m.map(u => u.user.username)
-                if (u.user.username == dmusr.username)
-                {
-					msg.delete();
-                    // Thanks Angel Bot#6208 for the code here
-                    let msg = args; // making a new variable with the argument list
-                    msg.shift(); // removing the first argument from the list
-                    if (args.length < 1) {
-                        msg.author.send("Make sure that there is text or a ping!")
-                    } else {
-                        const toSend = msg.join(" ");
-                        console.log(`${msg.author.username} sent "${toSend}" to ${dmusr.username}`);
-                        dmusr.send("[USER REQUESTED] " + toSend) // sending the list as a string with spaces seperating the items
-                            .catch(() => msg.author.send(`I could not send that DM to <@!${dmusr.id}>, this might be because they have DMs off or it was just a random error. Please try again later!`));
-                    }
-                }
-                else
-                {
-                    msg.author.send("That user isn't in this Server! You can't DM them.");
-                    console.log(`DM User not in Server.`);
-                }
-            })
+
+            bot.guild.cache.array().members.cache.forEach(member => console.log(member.user.username));
+            // list.members.fetch().then(m => {
+            //     let members = m.map(u => u.user.username)
+            //     if (u.user.username == dmusr.username)
+            //     {
+			// 		msg.delete();
+            //         // Thanks Angel Bot#6208 for the code here
+            //         let msg = args; // making a new variable with the argument list
+            //         msg.shift(); // removing the first argument from the list
+            //         if (args.length < 1) {
+            //             msg.author.send("Make sure that there is text or a ping!")
+            //         } else {
+            //             const toSend = msg.join(" ");
+            //             console.log(`${msg.author.username} sent "${toSend}" to ${dmusr.username}`);
+            //             dmusr.send("[USER REQUESTED] " + toSend) // sending the list as a string with spaces seperating the items
+            //                 .catch(() => msg.author.send(`I could not send that DM to <@!${dmusr.id}>, this might be because they have DMs off or it was just a random error. Please try again later!`));
+            //         }
+            //     }
+            //     else
+            //     {
+            //         msg.author.send("That user isn't in this Server! You can't DM them.");
+            //         console.log(`DM User not in Server.`);
+            //     }
+            // })
         }
     }
 }

83d07f0beb6482a5600c7c2834272429f62f8b6f
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2022-10-07 08:24:50 -0400

    bringing some old back into the new :3

--- commands/wip.js
+++ commands/wip.js
@@ -2,6 +2,6 @@ module.exports = {
     "name": "wip",
     "description": "Sorry! This command is currently in progress and will not work.",
     execute(msg, args, config) {
-        msg.channel.send(`<@${msg.author.id}>, this command is not finished yet. Sorry!`) // old reply moment
+        msg.channel.send(`<@${msg.author.id}>, **[An error has appeared!]**\n**Reason:** 'This command is not finished and will not work correctly.`) // old reply moment
     }
 }
